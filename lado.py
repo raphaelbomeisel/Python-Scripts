e898305ef9ee7a7cc86636380a13eb9c089456a2
raphaelbomeisel <raphaelcob1@al.insper.edu.br>
2015-03-19 09:34:32 -0300

    Início de projeto

---
+++ lado.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 19 08:26:54 2015
+
+@author: Raphael
+"""
+
+def q(lado):
+    area=lado**2
+    return area
+
+lado=float(input("Qual o lado do quadrado?"))
+
+print(q(lado))
+    
+    
